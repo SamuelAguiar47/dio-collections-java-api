2ac4c2286d1ed96badb82750a58305b7a1ab5975
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2024-07-11 17:15:27 -0300

    Realização de testes de ordenação para o Collection Set

--- src/main/java/set/Ordenacao/CadastroProdutos.java
+++ src/main/java/set/Ordenacao/CadastroProdutos.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 public class CadastroProdutos {
     private Set<Produto> produtoSet;
 
-    public CadastroProdutos(Set<Produto> produtoSet) {
+    public CadastroProdutos() {
         this.produtoSet = new HashSet<>();
     }
 
@@ -24,5 +24,17 @@ public class CadastroProdutos {
         Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
         produtosPorPreco.addAll(produtoSet);
         return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d, 5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d, 10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d, 2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d, 2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
     }
 }
